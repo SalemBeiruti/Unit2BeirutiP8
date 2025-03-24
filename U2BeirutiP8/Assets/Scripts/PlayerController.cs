01274a3a73de28ce3f10a58af2242d83b765ce6f
Salem Beiruti <726709@student.dusd.net>
2025-03-24 15:24:55 -0700

    Finished game and started challenges
    made animals spawn and die on collision

--- U2BeirutiP8/Assets/Scripts/PlayerController.cs
+++ U2BeirutiP8/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float verticalInput;
+    public float speed = 20.0f;
     public float xRange = 10.0f;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -29,7 +30,8 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
